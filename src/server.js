44a8c8d3140b7f9f923ea535550acaebb84f6688
imyroslav <imyroslav@ukr.net>
2025-09-29 12:01:12 +0700

    routes-created

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import pino from "pino";
 import pinoHttp from "pino-http";
-import { getEnvVar } from './utils/getEnvVar.js';
+import { getEnvVar } from "./utils/getEnvVar.js";
+import { getAllContacts, getContactById } from "./services/contacts.js";
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+
+const PORT = Number(getEnvVar("PORT", "3000"));
 
 
 export function setupServer() {
@@ -13,40 +15,69 @@ export function setupServer() {
 
   app.use(express.json());
 
-// Налаштування CORS  
+ 
   app.use(cors());
 
-// Налаштування логгера pino  
   const logger = pino();
   app.use(pinoHttp({ logger }));
 
-  //global middleware
-  app.use((req, res, next) => {
-    console.log("Global Middleware");
-    next();
-  });
+  // //global middleware
+  // app.use((req, res, next) => {
+  //   console.log("Global Middleware");
+  //   next();
+  // });
 
-  // local middleware
-  function middlewareLocal(req, res, next) {
-    console.log("local middleware");
-    next();
-  }
+  // // local middleware
+  // function middlewareLocal(req, res, next) {
+  //   console.log("local middleware");
+  //   next();
+  // }
 
 
-// Ваші маршрути тут (приклад)  
+// Root route
   app.get('/', (req, res) => {
     res.send('Сервер працює!');
   });
 
-  app.get('/books', middlewareLocal, (req, res) => {
+  app.get('/books', (req, res) => {
     res.send('Books are here');
   });
 
-// Обробка неіснуючих роутів 
+  // All contacts route
+  app.get("/contacts", async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  });
+
+  // Contact by id route
+  app.get("/contacts/:contactId", async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);  
+
+    if (!contact) {
+	    res.status(404).json({
+      message: "Contact not found"
+	  });
+	  return;
+    }
+    
+    res.status(200).json({
+	    message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+    next();
+  });
+
+
+  // Not existing route handler
   app.use((req, res) => {
     res.status(404).json({
-  message: "Not found this route",
-}
+    message: "Not found this route",
+  }
 );
   });
 
