91eb77f9e668479b3333d0454c4be22b67b01f75
imyroslav <imyroslav@ukr.net>
2025-10-01 21:54:34 +0700

    error-handling

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
-import { getAllContacts, getContactById } from "./services/contacts.js";
+import createHttpError from "http-errors";
+import { getAllContacts, getContactById } from "../services/contacts.js";
 
 // All contacts controller
 export const getContactsController = async (req, res) => {
@@ -13,13 +14,12 @@ export const getContactsController = async (req, res) => {
 };
 
 // Contact by id controller
-export const getContactByIdController = async (req, res, next) => {
+export const getContactByIdController = async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);  
 
     if (!contact) {
-        next(new Error("Contact not found :("));
-	    return;
+        throw createHttpError(404, "Contact not found :( ");
     }
     
     res.status(200).json({
