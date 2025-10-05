c5f14c7bd98847f7b27f34cf386a97b1fa5b8f97
imyroslav <imyroslav@ukr.net>
2025-10-05 22:37:13 +0700

    add-sorting

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,14 +6,20 @@ import {
     patchContact,
     deleteContact,
 } from "../services/contacts.js";
-import { parsePaginationParams } from "../utils/parsePaginationParams.js"
+import { parsePaginationParams } from "../utils/parsePaginationParams.js";
+import { parseSortParams } from "../utils/parseSortParams.js";
 
 // Get all contacts controller
 export const getContactsController = async (req, res) => {
     const { page, perPage } = parsePaginationParams(req.query);
+
+    const { sortBy, sortOrder } = parseSortParams(req.query);
+
     const contacts = await getAllContacts({
     page,
     perPage,
+    sortBy,
+    sortOrder,
   });
 
     res.status(200).json({
