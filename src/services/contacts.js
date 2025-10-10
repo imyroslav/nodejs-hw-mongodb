f3a1b9368ffa28c376eb910b59ec4e9a8afefa74
imyroslav <imyroslav@ukr.net>
2025-10-11 02:11:16 +0700

    add-authorization-2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,12 +8,13 @@ export const getAllContacts = async ({
   sortOrder = SORT_ORDER.ASC,
   sortBy = "_id", 
   filter = {},
+  userId,
   }) => {
 
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
+  const contactsQuery = ContactsCollection.find({userId});
 
   if (filter.contactType) {
     contactsQuery.where( "contactType" ).equals(filter.contactType);
